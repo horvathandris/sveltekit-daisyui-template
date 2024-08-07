9b8224d1399c581116f6d9d45bcdbbd8bf185769
Horváth Andris <horvath.andris@transferwise.com>
2024-08-07 21:06:30 +0100

    storybook eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslintPluginSvelte from "eslint-plugin-svelte";
 import js from "@eslint/js";
 import tsEslintParser from "@typescript-eslint/parser";
 import svelteParser from "svelte-eslint-parser";
+import eslintPluginStorybook from "eslint-plugin-storybook";
 
 export default [
   {
@@ -10,6 +11,7 @@ export default [
   js.configs.recommended,
   ...eslintPluginSvelte.configs["flat/recommended"],
   ...eslintPluginSvelte.configs["flat/prettier"],
+  ...eslintPluginStorybook.configs["flat/recommended"],
   {
     files: ["**/*.svelte"],
     languageOptions: {
