37162e49be51e3d67ef461e0198d1a2f02f2d15a
Horváth Andris <horvath.andris@transferwise.com>
2024-08-07 21:00:19 +0100

    add storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,28 @@
 import eslintPluginSvelte from "eslint-plugin-svelte";
 import js from "@eslint/js";
 import tsEslintParser from "@typescript-eslint/parser";
+import svelteParser from "svelte-eslint-parser";
 
 export default [
-	{
-		ignores: ["**/.svelte-kit/"],
-	},
-	{
-		languageOptions: {
-			parser: tsEslintParser,
-		},
-	},
-	js.configs.recommended,
-	...eslintPluginSvelte.configs["flat/recommended"],
-	...eslintPluginSvelte.configs["flat/prettier"],
-	{
-		files: ["**/*.ts", "**/*.tsx", "**/*.svelte"],
-	},
+  {
+    ignores: ["**/.svelte-kit/"],
+  },
+  js.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsEslintParser,
+      },
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsEslintParser,
+    },
+  },
 ];
