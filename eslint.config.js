0cd27200e4362a36c4d5165fd02178e1850efba7
Andris Horváth <59876688+horvathandris@users.noreply.github.com>
2024-11-05 10:42:48 +0100

    chore: bump svelte-check (#9)

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,29 @@
 import sveltePlugin from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
 import svelteParser from "svelte-eslint-parser";
 import storybookPlugin from "eslint-plugin-storybook";
 import tailwindPlugin from "eslint-plugin-tailwindcss";
 
 export default [
-  {
-    ignores: ["**/.svelte-kit/"],
-  },
   js.configs.recommended,
   ...ts.configs.recommended,
   ...storybookPlugin.configs["flat/recommended"],
   ...tailwindPlugin.configs["flat/recommended"],
   ...sveltePlugin.configs["flat/recommended"],
+  prettier,
   ...sveltePlugin.configs["flat/prettier"],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   {
     files: ["**/*.svelte"],
     languageOptions: {
@@ -30,5 +38,8 @@ export default [
     languageOptions: {
       parser: tsParser,
     },
+  },
+  {
+    ignores: ["build/", ".svelte-kit/", "dist/"],
   },
 ];
